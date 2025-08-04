5144e47e92524d35532ac27bc1de31ee9ed43701
KimDongYun <67079959+ehddbs3408@users.noreply.github.com>
2025-08-05 01:55:10 +0900

    Font Upload

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Rigidbody rb;
+
+    private float moveSpeed = 5f;
+
+    private Vector2 movement;
+
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector3(movement.x, movement.y, 0);
+    }
     void Update()
     {
-        
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        movement = new Vector2(x, y).normalized * moveSpeed;
     }
 }
